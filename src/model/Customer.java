97fae7eac4e92915e72bb7567a144d6eeaf05216
Kira060200 <constantin.gab02@gmail.com>
2021-03-05 15:27:27 +0200

    minimal implementations

---
+++ src/model/Customer.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Customer {
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String CNP;
+    private String email;
+
+    public Customer(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getCNP() {
+        return CNP;
+    }
+
+    public void setCNP(String CNP) {
+        this.CNP = CNP;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
