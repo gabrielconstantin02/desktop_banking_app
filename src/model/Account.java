d753f0a5c6d78d83c6ff6d4b717800acbeed5210
Kira060200 <constantin.gab02@gmail.com>
2021-03-05 15:37:35 +0200

    class template for account

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,5 +1,49 @@
 package model;
 
 public class Account {
-    private String iban
+    private String iban;
+    private String currency;
+    private String bic;
+    private float balance;
+
+    public Account(String iban, String currency, String bic) {
+        this.iban = iban;
+        this.currency = currency;
+        this.bic = bic;
+    }
+    public Account(){
+
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getBic() {
+        return bic;
+    }
+
+    public void setBic(String bic) {
+        this.bic = bic;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
 }
